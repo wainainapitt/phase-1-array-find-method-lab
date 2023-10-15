f9e99f3ce10c5960f01a529952744dac8594a9fd
peter <pngugi.wainaina@gmail.com>
2023-10-15 10:06:13 +0300

    Array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+superbowlWin = (record) => {
+
+    const result = record.find( record => record.result === "W" );
+
+  //  ✔ returns undefined if the record has no win objects
+    return !!result ? result.year : undefined;
+  }
